c47d463a2b0445bfe3ffb8782063e5630cc122bd
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-05-09 15:30:09 -0300

    feat: req 03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    can_process = True
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            can_process = False
+
+    if can_process:
+        process_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data,
+        }
+
+        instance.enqueue(process_data)
+        print(process_data, file=sys.stdout)
 
 
 def remove(instance):
