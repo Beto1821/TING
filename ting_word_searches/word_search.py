585271cc1cce280206ec0f73a805dfe4f2a9193b
Adalberto Ramos Ribeiro <beto1821@uol.com.br>
2023-05-10 22:17:21 -0300

    feat req07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = []
+    for i in range(len(instance)):
+        item = instance.search(i)
+        found_occurrences = []
+
+        #  itera sobre as linhas do arquivo atual
+        #  e retorna o índice e a linha atual.
+        for j, line in enumerate(item["linhas_do_arquivo"]):
+            # Palavra buscada
+            if word.lower() in line.lower():
+                found_occurrences.append({"linha": j})
+
+        if found_occurrences:
+            # append lista de ocorrencias
+            occurrences.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": found_occurrences
+            })
+
+    return occurrences
 
 
 def search_by_word(word, instance):
